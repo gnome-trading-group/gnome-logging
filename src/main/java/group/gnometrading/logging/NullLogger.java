97bfea31d4124b7289c33646b7681ee2383f29bd
mprey <masonprey7@gmail.com>
2025-10-16 20:01:48 -0400

    feat(*): add a NullLogger impl

---
+++ src/main/java/group/gnometrading/logging/NullLogger.java
@@ -0,0 +1,44 @@
+package group.gnometrading.logging;
+
+public class NullLogger implements Logger {
+
+    @Override
+    public void log(LogMessage msg) {
+
+    }
+
+    @Override
+    public void log(LogMessage msg, long v1) {
+
+    }
+
+    @Override
+    public void log(LogMessage msg, long v1, long v2) {
+
+    }
+
+    @Override
+    public void log(LogMessage msg, long v1, long v2, long v3) {
+
+    }
+
+    @Override
+    public void log(LogMessage msg, long v1, long v2, long v3, long v4) {
+
+    }
+
+    @Override
+    public void log(LogMessage msg, long v1, long v2, long v3, long v4, long v5) {
+
+    }
+
+    @Override
+    public void log(LogMessage msg, long v1, long v2, long v3, long v4, long v5, long v6) {
+
+    }
+
+    @Override
+    public void logf(LogMessage msg, String format, Object... args) {
+
+    }
+}
