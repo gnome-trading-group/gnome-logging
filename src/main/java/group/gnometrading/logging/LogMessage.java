9e33ac2bbda2e808f4f16ab0518a5f7b07ac85f3
mprey <masonprey7@gmail.com>
2025-10-28 10:34:04 -0400

    fix(*): modify socket messages

--- src/main/java/group/gnometrading/logging/LogMessage.java
+++ src/main/java/group/gnometrading/logging/LogMessage.java
@@ -1,11 +1,20 @@
 package group.gnometrading.logging;
 
 public enum LogMessage {
+
+    /** Socket related messages */
+    SOCKET_CONNECTING,
     SOCKET_CONNECTED,
+
+    SOCKET_CONNECT_FAILED,
+    SOCKET_CONNECT_TIMED_OUT,
+
+    SOCKET_DISCONNECTING,
     SOCKET_DISCONNECTED,
-    SOCKET_SILENCE_TIMEOUT,
+    SOCKET_SILENCE_TIMED_OUT, // no data received for configured period
     SOCKET_RECONNECTING,
-    SOCKET_RECONNECT_FAILURE,
+
+    /** General messages */
     FATAL_ERROR_EXITING,
     UNKNOWN_ERROR,
     DEBUG,
